8b5e88ca13596a25fcc8419f71fae6002dd11816
looseyi <13615033587@126.com>
2017-03-19 01:58:54 +0800

    Initial Commit

---
+++ SwiftUserDefault/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftUserDefault
+//
+//  Created by Edmond on 19/03/2560 BE.
+//  Copyright © 2560 BE Edmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
